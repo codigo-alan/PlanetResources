fd996ff30637d5c8d82aab57f82a603ba849acc3
alan.marcos.7e5 <alan.marcos.7e5@itb.cat>
2023-02-08 19:52:42 +0100

    Agregadas dependencias

--- app/src/main/java/com/example/planetresources/view/MainActivity.kt
+++ app/src/main/java/com/example/planetresources/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.planetresources
+package com.example.planetresources.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.planetresources.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
